188e2b98d10ce072de494f321b2058f9ac8fcf42
drewsmith1csu <drew.smith1@csuglobal.edu>
2025-07-12 15:16:44 -0600

    Create shape.java

---
+++ shape.java
@@ -0,0 +1,12 @@
+package Mod4;
+
+public abstract class Shape {
+
+	/*
+	 * This is the abstract class, it will have 2 abstract methods to be over written 
+	 * This class is the class everything will be referencing. 
+	 * 
+	 * */
+	abstract double surface_area();
+	abstract double volume();
+}
